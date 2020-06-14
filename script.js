e0c68426946bfac9c0573bf3cda432a19dfdba7b
Scott Griffin <mscottgriffin@gmail.com>
2020-06-14 14:54:07 -0400

    added variables and began pseudocode in script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('hello world')
+
+//VARIABLES
+var saveButton = document.querySelector('saveBtn');
+var newRow = document.querySelector('row');
+var hour = document.querySelector('hour')//rename
+///FUNCTIONS
+
+//function saveNote
+//when save button is clicked, save contents and hour to localstorage
+// if there is already a note in local storage for that hour, update it or clear it before saving
+
+
+//function colorize
+//if hour = currenttime, description is red
+//if past, description grey
+//if future, description green
+
+
+//EVENT LISTENERS
