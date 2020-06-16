694301b4ad3c28e380ba3dbb8f9c2f33e56bcf2c
Scott Griffin <mscottgriffin@gmail.com>
2020-06-16 11:36:57 -0400

    changed div to textarea t enable text.

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
       var rowEl = $('<div>').addClass('row');
       var timeBlockEl = $('<div>').addClass('time-block');
       var hourCol = $('<div>').addClass('col-2 hour');
-      var descriptionEl = $('<div>').addClass('col-9 description');
+      var descriptionEl = $('<textarea>').addClass('col-9 description');
       var buttonColEl = $('<button>').addClass('col-1 saveBtn');
 
       if (workHours[i] == nowHour){
